cf756a2d71ef7d9197f79a7b7f34ecf6910f0174
thedreamer67 <carpenterswift@gmail.com>
2021-03-04 17:29:50 +0800

    nil123

--- app/src/main/java/com/example/lockerxlogin/MainActivity.java
+++ app/src/main/java/com/example/lockerxlogin/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    //testshuana
+    //testshuana123
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
