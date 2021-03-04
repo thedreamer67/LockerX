36d797a0a4077b4ddbae4b9493e1b58e7d3f4a1b
Nicholas <nicholaskhong@gmail.com>
2021-03-04 16:47:59 +0800

    Test

--- app/src/main/java/com/example/lockerxlogin/Login.java
+++ app/src/main/java/com/example/lockerxlogin/Login.java
@@ -19,7 +19,7 @@ public class Login extends AppCompatActivity {
     ProgressBar LProgressBar;
     Button LLoginBtn;
     FirebaseAuth fAuth;
-
+    //Hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
