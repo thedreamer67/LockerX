2baa295d93055ce8350e07a18327b4b8ede7bf65
d24ab <72213195+d24ab@users.noreply.github.com>
2021-03-04 16:30:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lockerxlogin/Login.java
@@ -0,0 +1,72 @@
+package com.example.lockerxlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    EditText LEmail, LPassword;
+    TextView LLoginPage, LRegisterBtn;
+    ProgressBar LProgressBar;
+    Button LLoginBtn;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        LEmail = findViewById(R.id.LEmail);
+        LPassword = findViewById(R.id.LPassword);
+        LLoginPage = findViewById(R.id.LLoginText);
+        LProgressBar = findViewById(R.id.LprogressBar);
+        LLoginBtn= findViewById(R.id.LLoginBtn);
+        LRegisterBtn = findViewById(R.id.LRegister);
+
+        LLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                String email = LEmail.getText().toString().trim();
+                String password = LPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    LEmail.setError("Email is required");
+                    return;
+
+                }
+                if (TextUtils.isEmpty(password)) {
+                    LPassword.setError("Password is required");
+                    return;
+                }
+                if (password.length() < 6) { //Change this constraint TODO{
+                    LPassword.setError("Password must have at least a capital letter and a symbol");
+                    return;
+                }
+
+                LProgressBar.setVisibility(View.VISIBLE);
+
+            }
+         });
+
+
+
+        LRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/lockerxlogin/Register.java
@@ -0,0 +1,99 @@
+package com.example.lockerxlogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
+
+public class Register extends AppCompatActivity {
+    EditText mEmail, mPassword;
+    TextView mLoginBtn;
+    Button mRegisterBtn, log;
+    ProgressBar mprogressBar;
+    FirebaseAuth fAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        mEmail = findViewById(R.id.mEmail);
+        mPassword = findViewById(R.id.mPassword);
+        mLoginBtn = findViewById(R.id.LoginFromRegisterBtn);
+        mprogressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+        mRegisterBtn = findViewById(R.id.mRegisterBtn);
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is required");
+                    return;
+
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if (password.length() < 6){//Change this constraint TODO{
+                    mPassword.setError("Password must have at least a capital letter and a symbol");
+                    return;
+                }
+
+                mprogressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(Register.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+
+        });
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+
+            }
+
+        });
+
+
+
+
+    }
+}
