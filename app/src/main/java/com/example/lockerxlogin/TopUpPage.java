21eb534fde39651ac99ef7df9ee58a7c593d72a4
d24ab <72213195+d24ab@users.noreply.github.com>
2021-03-06 01:56:13 +0800

    asd

---
+++ app/src/main/java/com/example/lockerxlogin/TopUpPage.java
@@ -0,0 +1,4 @@
+package com.example.lockerxlogin;
+
+public class TopUpPage {
+}
--- app/src/main/java/com/example/lockerxlogin/fragment/WalletFragment.java
+++ app/src/main/java/com/example/lockerxlogin/fragment/WalletFragment.java
@@ -1,21 +1,39 @@
 package com.example.lockerxlogin.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.lockerxlogin.R;
 import com.example.lockerxlogin.ui.wallet.WalletViewModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WalletFragment extends Fragment {
-
+    TextView walletBalance, Title;
+    Button topUpButton;
     private WalletViewModel mViewModel;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    DatabaseReference reff;
+    SpannableStringBuilder builder = new SpannableStringBuilder();
+
+
 
     public static WalletFragment newInstance() {
         return new WalletFragment();
@@ -24,8 +42,30 @@ public class WalletFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wallet, container, false);
+        View myView = inflater.inflate(R.layout.fragment_wallet, container, false);
+        walletBalance = myView.findViewById(R.id.walletBalance);
+        topUpButton = myView.findViewById(R.id.topUpButton);
+        reff = FirebaseDatabase.getInstance().getReference().child("User").child("12345678");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String balance = snapshot.child("walletBalance").getValue().toString();
+                String FirstLine = ("You have \n$ ");
+                walletBalance.setText(FirstLine + balance);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+        return myView;
     }
+
+
 
    /* @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
