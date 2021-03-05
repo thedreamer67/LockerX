ccee95f767120e1165cd770d7bf0b9ac53597545
Nicholas <nicholaskhong@gmail.com>
2021-03-06 00:50:16 +0800

    bug fixes

--- app/src/main/java/com/example/lockerxlogin/MainActivity.java
+++ app/src/main/java/com/example/lockerxlogin/MainActivity.java
@@ -54,11 +54,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 
+
         search = (Button) findViewById(R.id.searchBT);
         connect = (Button) findViewById(R.id.connectBT);
+
         listView = (ListView) findViewById(R.id.listview);
-
-
 
         if (savedInstanceState != null) {
             ArrayList<BluetoothDevice> list = savedInstanceState.getParcelableArrayList(DEVICE_LIST);
