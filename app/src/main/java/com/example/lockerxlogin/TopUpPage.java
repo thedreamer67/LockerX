cd2343301aad2587fcdcb9545f97d17a6f1b82fe
d24ab <72213195+d24ab@users.noreply.github.com>
2021-03-06 03:27:04 +0800

    Top Up

--- app/src/main/java/com/example/lockerxlogin/TopUpPage.java
+++ app/src/main/java/com/example/lockerxlogin/TopUpPage.java
@@ -2,7 +2,9 @@ package com.example.lockerxlogin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,7 @@ import com.google.firebase.database.ValueEventListener;
 public class TopUpPage extends AppCompatActivity {
     Button topUp;
     TextView topUpBalance;
+    Spinner paymentType;
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
     DatabaseReference reff;
@@ -28,6 +31,13 @@ public class TopUpPage extends AppCompatActivity {
 
         topUpBalance = findViewById(R.id.topUpBalance);
         topUp = findViewById(R.id.topUp);
+        paymentType= findViewById(R.id.paymentType);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(TopUpPage.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.name));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentType.setAdapter(myAdapter);
+
 
         reff = FirebaseDatabase.getInstance().getReference().child("User").child("12345678");
         reff.addValueEventListener(new ValueEventListener() {
