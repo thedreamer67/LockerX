fc71f97868dac2f3995bacaed46d0ed650cf3ad9
thedreamer67 <carpenterswift@gmail.com>
2021-03-07 20:10:20 +0800

    added a comment in TempDataStorage

--- app/src/main/java/com/example/lockerxlogin/TempDataStorage.java
+++ app/src/main/java/com/example/lockerxlogin/TempDataStorage.java
@@ -1,5 +1,6 @@
 package com.example.lockerxlogin;
 
+//this class is for retrieving data and being able to use the data outside of the onDataChange() method
 public class TempDataStorage {
     private String str;
     private long longNum;
